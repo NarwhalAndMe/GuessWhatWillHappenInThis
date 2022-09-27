4842660f85fed5683a1b8122103b7f495e021899
NarwhalAndMe <98322440+NarwhalAndMe@users.noreply.github.com>
2022-09-27 12:17:32 -0400

    fixed "hopefully"

--- src/main/java/fr/anatom3000/gwwhit/command/Commands.java
+++ src/main/java/fr/anatom3000/gwwhit/command/Commands.java
@@ -19,6 +19,7 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.Identifier;
+import net.minecraft.sound.SoundEvents;
 
 public class Commands {
     private Commands() {
@@ -97,6 +98,13 @@ public class Commands {
                                     return 1;
                                 })
                         )
+                )
+                .then(CommandManager.literal("hi")
+                        .executes(context -> {
+                            context.getSource().getPlayer().playSound(SoundEvents.BLOCK_WOOL_PLACE, 1.0f, 1.0f);
+                            context.getSource().getPlayer().sendMessage(new LiteralText("hi"), true);
+                            return 1;
+                        })
                 )
         ));
     }
